83be12924e0799f4e1fd9b28e20238a8c2e7a0a1
fousseni <foussenicoulibalypro@gmail.com>
2018-11-15 14:29:17 +0100

    Updates

---
+++ precache-manifest.f031d2eff216ba2cf59d8f1507a0fd94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87b25011cbf610a5bac8",
+    "url": "/static/css/main.98225a98.chunk.css"
+  },
+  {
+    "revision": "87b25011cbf610a5bac8",
+    "url": "/static/js/main.87b25011.chunk.js"
+  },
+  {
+    "revision": "a778b46315e085407325",
+    "url": "/static/js/1.a778b463.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "078ae3e623bbf6d865776fbedc4eab29",
+    "url": "/index.html"
+  }
+];
